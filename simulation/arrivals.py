43758f9386be1c58b08e6fb81f4b2f61be9bead6
Janis Heuel <janis-heuel@gmx.de>
2025-08-19 13:40:23 +0200

    add simulation

---
+++ simulation/arrivals.py
@@ -0,0 +1,131 @@
+"""
+Simulate arrival times for stations and random locations within the seismic network
+TODO: Allow single phase arrivals in create event
+"""
+
+import random
+import pandas as pd
+import numpy as np
+from typing import Optional
+
+from obspy.geodetics import degrees2kilometers, locations2degrees
+from obspy import UTCDateTime
+
+
+def create_event(
+    stations: pd.DataFrame,
+    latitude_range: tuple,
+    longitude_range: tuple,
+    depth_range: tuple,
+    origin_time: UTCDateTime,
+    vp: float = 4500,
+    vs: Optional[float] = None,
+    min_num_stations: int = 4,
+) -> (pd.DataFrame, tuple):
+    if not vs:
+        vs = vp / np.sqrt(3)
+
+    # Simulate location of random earthquake
+    latitude = np.random.uniform(low=min(latitude_range), high=max(latitude_range))
+    longitude = np.random.uniform(low=min(longitude_range), high=max(longitude_range))
+    depth = np.random.uniform(low=min(depth_range), high=max(depth_range))
+
+    # Select stations that have a pick by choosing station indices from all stations
+    station_idx = []
+    num_stations = np.random.randint(min_num_stations, len(stations))
+    all_idx = list(stations.index)
+    for i in range(num_stations):
+        idx = np.random.choice(all_idx)
+        all_idx.remove(idx)
+        station_idx.append(idx)
+
+    # Create arrivals for each station
+    arrivals = {"trace_id": [], "peak_time": [], "phase": [], "peak_value": []}
+    for stat_idx in station_idx:
+        # Compute traveltimes
+        distance_degree = locations2degrees(
+            lat1=stations.loc[stat_idx, "latitude"],
+            long1=stations.loc[stat_idx, "longitude"],
+            lat2=latitude,
+            long2=longitude,
+        )
+        epi_distance_m = degrees2kilometers(degrees=distance_degree) * 1000
+        hypo_distance_m = np.sqrt(
+            (stations.loc[stat_idx, "elevation_m"] + depth) ** 2 + epi_distance_m**2
+        )
+
+        for phase, v in zip(["P", "S"], [vp, vs]):
+            traveltime = hypo_distance_m / v
+            arrivals["phase"].append(phase)
+            arrivals["trace_id"].append(stations.loc[stat_idx, "station_name"])
+            arrivals["peak_time"].append(origin_time + traveltime)
+            arrivals["peak_value"].append(np.random.uniform(low=0.2, high=1.0))
+
+    return pd.DataFrame(arrivals), (latitude, longitude, depth)
+
+
+def add_random_picks(event_df: pd.DataFrame, percentage: float = 15) -> pd.DataFrame:
+    """
+    Adding fake picks to event_df
+
+    :param event_df:
+    :param percentage:
+    :return:
+    """
+    stations = list(
+        set(event_df["trace_id"])
+    )  # Create list with all stations from event_df
+    starttime, endtime = (
+        min(event_df["peak_time"]) - 30,
+        max(event_df["peak_time"]) + 30,
+    )  # Select time range from all times (add 30 seconds)
+    add_picks = int(
+        len(event_df) * percentage / 100
+    )  # Number of how many events will be added
+
+    # Transform pandas dataframe to dictionary
+    event_dct = event_df.to_dict(orient="list")
+
+    # Create random picks with low probabilities
+    for i in range(add_picks):
+        event_dct["trace_id"].append(random.choice(stations))
+        event_dct["peak_time"].append(
+            starttime + np.random.uniform(low=0, high=endtime - starttime)
+        )
+        event_dct["phase"].append(random.choice(["P", "S"]))
+        event_dct["peak_value"].append(np.random.uniform(low=0.2, high=0.5))
+
+    return pd.DataFrame(event_dct)
+
+
+if __name__ == "__main__":
+    stations = pd.read_csv("../metadata/stations.csv")
+    lat_max = 48.90599442
+    lat_min = 48.89217758
+    lon_max = 7.938097954
+    lon_min = 7.914076328
+    max_depth = 3052.23012
+    min_depth = 1788.609028
+    starttime = UTCDateTime("20250401 15:30")
+
+    vp = 4500
+    vs = vp / np.sqrt(3)
+
+    for i in range(10):
+        origin_time = starttime + np.random.randint(0, 300)
+        df, _ = create_event(
+            stations=stations,
+            latitude_range=(lat_min, lat_max),
+            longitude_range=(lon_min, lon_max),
+            depth_range=(min_depth, max_depth),
+            origin_time=origin_time,
+            vp=vp,
+            vs=vs,
+        )
+        if i == 0:
+            df_events = df
+        else:
+            df_events = pd.concat(objs=[df_events, df])
+
+    df_events = add_random_picks(event_df=df_events)
+    print()
---
+++ simulation/utils.py
@@ -0,0 +1,167 @@
+from typing import Optional
+
+import pathlib
+import obspy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def moveout(
+    catalog: obspy.Catalog,
+    station_json: pd.DataFrame,
+    ax_lat: Optional[plt.axes] = None,
+    ax_lon: Optional[plt.axes] = None,
+    csv_pick_files: Optional[list] = None,
+):
+    """
+    Plotting moveout curve from catalog along time and latitude / longitude
+    """
+    # Set color for each line
+    colors = [
+        "tab:blue",
+        "tab:orange",
+        "tab:green",
+        "tab:red",
+        "tab:purple",
+        "tab:brown",
+        "tab:pink",
+        "tab:gray",
+        "tab:olive",
+        "tab:cyan",
+    ]
+    colors = colors * int(np.ceil(len(catalog) / len(colors)))
+
+    # If pick_files available plot all available picks in grey
+    csv_p_picks = []
+    csv_s_picks = []
+    csv_p_latitudes = []
+    csv_s_latitudes = []
+    csv_p_longitudes = []
+    csv_s_longitudes = []
+    if csv_pick_files:
+        for filename in csv_pick_files:
+            # Find location of station from station_json
+            station_code = pathlib.Path(filename).stem
+            try:
+                station_idx = list(station_json["id"]).index(station_code)
+            except ValueError:
+                continue
+            picks = pd.read_csv(filename)  # Read pick-file
+
+            # Loop over each entry in csv-file
+            for idx in range(len(picks)):
+                if picks.loc[idx, "phase"].lower() == "p":
+                    csv_p_picks.append(
+                        obspy.UTCDateTime(picks.loc[idx, "peak_time"]).datetime
+                    )
+                    csv_p_latitudes.append(station_json.loc[station_idx, "latitude"])
+                    csv_p_longitudes.append(station_json.loc[station_idx, "longitude"])
+                elif picks.loc[idx, "phase"].lower() == "s":
+                    csv_s_picks.append(
+                        obspy.UTCDateTime(picks.loc[idx, "peak_time"]).datetime
+                    )
+                    csv_s_latitudes.append(station_json.loc[station_idx, "latitude"])
+                    csv_s_longitudes.append(station_json.loc[station_idx, "longitude"])
+
+        # Plot picks in grey
+        if ax_lat:
+            ax_lat.scatter(
+                x=csv_p_picks,
+                y=csv_p_latitudes,
+                color="grey",
+                alpha=0.35,
+                rasterized=True,
+            )
+        if ax_lon:
+            ax_lon.scatter(
+                x=csv_p_picks,
+                y=csv_p_longitudes,
+                color="grey",
+                alpha=0.35,
+                rasterized=True,
+            )
+
+        # Plot S-picks
+        if ax_lat:
+            ax_lat.scatter(
+                x=csv_s_picks,
+                y=csv_s_latitudes,
+                color="grey",
+                alpha=0.35,
+                edgecolors=["k"] * len(csv_s_picks),
+                rasterized=True,
+            )
+        if ax_lon:
+            ax_lon.scatter(
+                x=csv_s_picks,
+                y=csv_s_longitudes,
+                color="grey",
+                alpha=0.35,
+                edgecolors=["k"] * len(csv_s_picks),
+                rasterized=True,
+            )
+
+    for idx, event in enumerate(catalog):
+        # origin_time = event.origins[-1].datetime
+        p_picks = []
+        s_picks = []
+        p_latitudes = []
+        p_longitudes = []
+        s_latitudes = []
+        s_longitudes = []
+        for pick in event.picks:
+            # Find latitude and longitude of station
+            station_code = (
+                f"{pick.waveform_id.network_code}.{pick.waveform_id.station_code}."
+                f"{pick.waveform_id.location_code}"
+            )
+            try:
+                station_idx = list(station_json["station_name"]).index(station_code)
+            except ValueError:
+                continue
+
+            if pick.phase_hint.lower() in ["p", "pg", "pn"]:
+                p_picks.append(pick.time.datetime)
+                p_latitudes.append(station_json.loc[station_idx, "latitude"])
+                p_longitudes.append(station_json.loc[station_idx, "longitude"])
+            elif pick.phase_hint.lower() in ["s", "sg", "sn"]:
+                s_picks.append(pick.time.datetime)
+                s_latitudes.append(station_json.loc[station_idx, "latitude"])
+                s_longitudes.append(station_json.loc[station_idx, "longitude"])
+
+        # Sort latitudes
+        # zipped = zip(p_latitudes, p_picks)
+
+        # Plot moveout
+        # ax.plot(p_picks,
+        #         p_latitudes,
+        #         color="grey",
+        #         linestyle="--",
+        #         linewidth=0.5)
+
+        # Plot P-picks
+        if ax_lat:
+            ax_lat.scatter(x=p_picks, y=p_latitudes, color=colors[idx], rasterized=True)
+        if ax_lon:
+            ax_lon.scatter(
+                x=p_picks, y=p_longitudes, color=colors[idx], rasterized=True
+            )
+
+        # Plot S-picks
+        if ax_lat:
+            ax_lat.scatter(
+                x=s_picks,
+                y=s_latitudes,
+                color=colors[idx],
+                edgecolors=["k"] * len(s_picks),
+                rasterized=True,
+            )
+        if ax_lon:
+            ax_lon.scatter(
+                x=s_picks,
+                y=s_longitudes,
+                color=colors[idx],
+                edgecolors=["k"] * len(s_picks),
+                rasterized=True,
+            )
