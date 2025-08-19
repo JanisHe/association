e293619cc82c602fa0b5bb610ddf0c6967463d45
Janis Heuel <janis-heuel@gmx.de>
2025-08-19 13:39:42 +0200

    inital commit

---
+++ core/imterfaces.py
@@ -0,0 +1,20 @@
+"""
+Function for different association algorithms:
+HARPA, PyOcto, and GaMMA
+"""
+
+# TODO: Input: csv file with picks, output catalog with events
+#       Columns of csv file: trace_id (network.station.location), peak_time (UTCDateTime ???), peak_value, phase
+#       csv file needs to be created before (i.e., concatenating files from all stations and consider overlapping in time)
+
+
+def interface_pyocto():
+    pass
+
+
+def interface_gamma():
+    pass
+
+
+def interface_harpa():
+    pass
